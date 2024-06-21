efba39af5aa6d0a17876630e0988f7e59392e920
Angkur Mondal <angkurmondal1998@gmail.com>
2024-06-21 19:34:15 +0600

    crud operations on task

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors({
 }));
 
 app.use(`${api}/tasklists`, taskListRouter);
-app.use(`${api}/tasks`, taskRouter);
+//app.use(`${api}/tasks`, taskRouter);
 
 
 await databaseConnection(process.env.DB_URL, process.env.DB_NAME);
--- routers/taskRouter.js
+++ routers/taskRouter.js
@@ -3,65 +3,47 @@ import { Task } from '../database/models/task.js';
 
 const taskRouter = express.Router();
 
-taskRouter.get('/', (req, res) => {
-    Task.find({})
-    .then((tasks) => {
+taskRouter.get('/', async (req, res) => {
+    try {
+        const tasks = await Task.find({ _taskListId: req.tasklistId });
         res.status(200).send(tasks);
-    })
-    .catch(err => {
+    } catch (err) {
         console.error(err);
-    });
+        res.status(500).send({ error: 'An error occurred while fetching tasks' });
+    }
 });
 
 taskRouter.get('/:taskId', (req, res) => {
-    console.log(req.params);
-
-    Task.findById(req.params.taskId)
-    .then((task) => {
-        res.status(200).send(task);
-    })
-    .catch(err => {
-        console.error(err);
-    });
-});
-
-taskRouter.post('/', (req, res) => {
-    
-    const task = new Task({
-        title: req.body.title,
-        _taskListId: req.body.tasklistId,
-        completed: req.body.completed
-    });
-
-    task.save()
-    .then(task => res.status(201).send(task))
-    .catch(err => {
-        console.error(err);
-    });
-});
-
-taskRouter.put('/:taskId', (req, res) => {
-    Task.findOneAndUpdate({_id: req.params.taskId}, {$set: req.body}, {new: true})
+    Task.findOne({_taskListId: req.tasklistId, _id: req.params.taskId})
     .then(task =>{
         res.status(200).send(task);
     })
-    .catch(err => console.error(err));
+    .catch(err => {
+        console.error(err);
+        res.status(500).send({ error: 'An error occurred while fetching a task' });
+    });
 });
 
 taskRouter.patch('/:taskId', (req, res) => {
-    Task.findOneAndUpdate({_id: req.params.taskId}, {$set: req.body}, {new: true})
+    Task.findOneAndUpdate({_taskListId: req.tasklistId, _id: req.params.taskId}, { $set: req.body }, { new: true })
     .then(task =>{
         res.status(200).send(task);
     })
-    .catch(err => console.error(err));
+    .catch(err => {
+        console.error(err);
+        res.status(500).send({ error: 'An error occurred while updating a task' });
+    });
 });
 
 taskRouter.delete('/:taskId', (req, res) => {
-    Task.findByIdAndDelete(req.params.taskId)
+    Task.findOneAndDelete({_taskListId: req.tasklistId, _id: req.params.taskId})
     .then(task =>{
         res.status(200).send(task);
     })
-    .catch(err => console.error(err));
+    .catch(err => {
+        console.error(err);
+        res.status(500).send({ error: 'An error occurred while deleting a task' });
+    });
 });
 
 export default taskRouter;
